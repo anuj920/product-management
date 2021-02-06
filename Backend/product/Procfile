54d2a5f677b7f5db7e1bb1ea3266f5f89c7d2751
Anuj Singh <anuj.singh@makerobos.com>
2021-02-07 00:21:22 +0530

    chnages

---
+++ Backend/product/Procfile
@@ -0,0 +1,1 @@
+web: gunicorn product.wsgi --log-file -
--- Frontend/product/src/environments/environment.prod.ts
+++ Frontend/product/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  serviceUrl: 'http://localhost:8000/management/'
+
 };
